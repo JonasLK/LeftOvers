a0e10bff01e7e6db2223e93b630c27c853ba6ab7
Ruben-nn <60340371+Ruben-nn@users.noreply.github.com>
2021-01-27 01:27:17 +0100

    folders + scenes + start character movement
    character movement (als je een character klikt laat hij zien welke tiles er voor hem beschikbaar zijn om naar te lopen)
    extra scenes + folders

---
+++ LeftOvers/Assets/Scripts/Movement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public bool canMove;
+
+    public GameObject[] tiles;
+    public float tileDetectionRange;
+    Transform moveToTile;
+
+    public float speed;
+    void Start()
+    {
+        tiles = GameObject.FindGameObjectsWithTag("Tile");
+    }
+
+    public void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        foreach (GameObject tile in tiles)
+        {
+            if (canMove == true)
+            {
+                if (Vector3.Distance(gameObject.transform.position, tile.transform.position) <= tileDetectionRange)
+                {
+                    tile.GetComponent<Renderer>().material.color = Color.green;
+                }
+            }
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        canMove = true;
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, tileDetectionRange);
+    }
+}
