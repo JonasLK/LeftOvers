ef76c0ebd462fcdb762be8b7348dc4547bb370ae
JonasLK <49680607+JonasLK@users.noreply.github.com>
2021-02-02 12:03:22 +0100

    Settings WIP

---
+++ LeftOvers/Assets/Scripts/UI/Settings/Volume.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class Volume : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+
+    public void UpdateMasterVolume(float volume)
+    {
+        audioMixer.SetFloat("MasterVolume", volume);
+    }
+
+    public void UpdateMusicVolume(float volume)
+    {
+        audioMixer.SetFloat("MusicVolume", volume);
+    }
+
+    public void UpdateSFXVolume(float volume)
+    {
+        audioMixer.SetFloat("SFXVolume", volume);
+    }
+}
