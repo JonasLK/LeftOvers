0b8aeb0d13f2859781a9279bb1e1f820cc7b029c
KoningSherwood <ramonzwiep@hotmail.com>
2021-02-03 12:27:58 +0100

    Camera Controller

---
+++ LeftOvers/Assets/Scripts/CameraController.cs
@@ -0,0 +1,147 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform cameraTransform;
+
+    public float normalSpeed;
+    public float fastSpeed;
+    public float movementSpeed;
+    public float movementTime;
+    public float rotationAmount;
+    public Vector3 zoomAmount;
+
+    public Vector3 newPosition;
+    public Quaternion newRotation;
+    public Vector3 newZoom;
+
+    public Vector3 dragStartPosition;
+    public Vector3 dragCurrentPosition;
+    public Vector3 rotateStartPosition;
+    public Vector3 rotateCurrentPosition;
+    void Start()
+    {
+        newPosition = transform.position;
+        newRotation = transform.rotation;
+        newZoom = cameraTransform.localPosition;
+    }
+
+    void Update()
+    {
+        HandleMovementInput();
+
+        HandleMouseInput();
+    }
+
+    void HandleMouseInput()
+    {
+        //Allows the player to zoom in and out with the mouse wheel.
+        if (Input.mouseScrollDelta.y != 0)
+        {
+            newZoom += Input.mouseScrollDelta.y * zoomAmount;
+        }
+
+        //Allows for moving the camera around by dragging with the mouse.
+        if (Input.GetMouseButtonDown(0))
+        {
+            Plane plane = new Plane(Vector3.up, Vector3.zero);
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            float entry;
+
+            if (plane.Raycast(ray, out entry))
+            {
+                dragStartPosition = ray.GetPoint(entry);
+            }
+        }
+        if (Input.GetMouseButton(0))
+        {
+            Plane plane = new Plane(Vector3.up, Vector3.zero);
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            float entry;
+
+            if (plane.Raycast(ray, out entry))
+            {
+                dragCurrentPosition = ray.GetPoint(entry);
+
+                newPosition = transform.position + dragStartPosition - dragCurrentPosition;
+            }
+        }
+
+        //Allows the player to rotate the camera by holding and dragging with the middle mouse button.
+        if (Input.GetMouseButtonDown(2))
+        {
+            rotateStartPosition = Input.mousePosition;
+        }
+        if (Input.GetMouseButton(2))
+        {
+            rotateCurrentPosition = Input.mousePosition;
+
+            Vector3 difference = rotateStartPosition - rotateCurrentPosition;
+
+            rotateStartPosition = rotateCurrentPosition;
+
+            newRotation *= Quaternion.Euler(Vector3.up * (-difference.x / 5f));
+        }
+    }
+
+    void HandleMovementInput()
+    {
+        //Allows the player to increase the movement speed of the camera for as long as they hold down left shift.
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            movementSpeed = fastSpeed;
+        }
+        else
+        {
+            movementSpeed = normalSpeed;
+        }
+
+        //Allows the player to move around with either the wasd keys or the arrow keys.
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            newPosition += (transform.forward * movementSpeed);
+        }
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            newPosition += (transform.forward * -movementSpeed);
+        }
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            newPosition += (transform.right * movementSpeed);
+        }
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            newPosition += (transform.right * -movementSpeed);
+        }
+
+        //Allows the player to rotate using the Q and E keys.
+        if (Input.GetKey(KeyCode.Q))
+        {
+            newRotation *= Quaternion.Euler(Vector3.up * rotationAmount);
+        }
+        if (Input.GetKey(KeyCode.E))
+        {
+            newRotation *= Quaternion.Euler(Vector3.up * -rotationAmount);
+        }
+
+        //Allows the player to zoom in and out with the R and F keys.
+        if (Input.GetKey(KeyCode.R))
+        {
+            newZoom += zoomAmount;
+        }
+        if (Input.GetKey(KeyCode.F))
+        {
+            newZoom -= zoomAmount;
+        }
+
+        transform.position = Vector3.Lerp(transform.position, newPosition, Time.deltaTime * movementTime);
+        transform.rotation = Quaternion.Lerp(transform.rotation, newRotation, Time.deltaTime * movementTime);
+        cameraTransform.localPosition = Vector3.Lerp(cameraTransform.localPosition, newZoom, Time.deltaTime * movementTime);
+    }
+}
