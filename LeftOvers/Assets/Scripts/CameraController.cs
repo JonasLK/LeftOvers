f433625ac264ebc8682fb63aac78d3644aeb9227
KoningSherwood <ramonzwiep@hotmail.com>
2021-02-08 14:19:52 +0100

    FighterUnit
    Unfinished

--- LeftOvers/Assets/Scripts/CameraController.cs
+++ LeftOvers/Assets/Scripts/CameraController.cs
@@ -21,6 +21,9 @@ public class CameraController : MonoBehaviour
     public Vector3 dragCurrentPosition;
     public Vector3 rotateStartPosition;
     public Vector3 rotateCurrentPosition;
+
+    public Vector3 upDownLimit;
+    public Vector3 leftRightLimit;
     void Start()
     {
         newPosition = transform.position;
@@ -102,23 +105,23 @@ public class CameraController : MonoBehaviour
             movementSpeed = normalSpeed;
         }
 
-        //Allows the player to move around with either the wasd keys or the arrow keys.
-        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
-        {
-            newPosition += (transform.forward * movementSpeed);
-        }
-        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
-        {
-            newPosition += (transform.forward * -movementSpeed);
-        }
-        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
-        {
-            newPosition += (transform.right * movementSpeed);
-        }
-        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
-        {
-            newPosition += (transform.right * -movementSpeed);
-        }
+            //Allows the player to move around with either the wasd keys or the arrow keys.
+            if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+            {
+                newPosition += (transform.forward * movementSpeed);
+            }
+            if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+            {
+                newPosition += (transform.forward * -movementSpeed);
+            }
+            if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+            {
+                newPosition += (transform.right * movementSpeed);
+            }
+            if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+            {
+                newPosition += (transform.right * -movementSpeed);
+            }
 
         //Allows the player to rotate using the Q and E keys.
         if (Input.GetKey(KeyCode.Q))
---
+++ LeftOvers/Assets/Scripts/UnitFighter.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitFighter : MonoBehaviour
+{
+    public float totalHealth;
+    private float currentHealth;
+    public float attackDamage;
+    public float enemyAttackDamage;
+
+    void Start()
+    {
+        currentHealth = totalHealth;
+    }
+
+    void Update()
+    {
+        isDead();
+        TakeDamage();
+    }
+
+    public bool isDead()
+    {
+        if (currentHealth <= 0f)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    /**
+    public bool CheckIfInRange()
+    {
+        if ()
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    
+    void Attacking()
+    {
+        if (CheckIfInRange())
+        {
+            //slap the shit outta them
+        }
+    }
+    **/
+
+    void TakeDamage()
+    {
+        if (isDead())
+        {
+            Death();
+            return;
+        }
+
+        currentHealth -= enemyAttackDamage;
+        //damage taken animation
+    }
+
+    void Death()
+    {
+        print("oh no i died");
+        //death animation
+        Destroy(gameObject, 3f);
+    }
+}
