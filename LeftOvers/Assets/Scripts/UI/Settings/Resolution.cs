d4025e2407fc45a2d559f3c1c33a1cf03f5183a4
JonasLK <49680607+JonasLK@users.noreply.github.com>
2021-02-09 11:14:58 +0100

    MM WIP

---
+++ LeftOvers/Assets/Scripts/UI/Settings/Resolution.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using TMPro;
+
+public class Resolution : MonoBehaviour
+{
+    /*public AudioMixer mixer;
+    public Resolution[] resolutions;
+    public int resInt;
+    public TMP_Dropdown resDrop;
+    private void Start()
+    {
+        mixer.SetFloat("MasterVol", Mathf.Log10(PlayerPrefs.GetFloat("masterVol")) * 20);
+        mixer.SetFloat("MusicVol", Mathf.Log10(PlayerPrefs.GetFloat("musicVol")) * 20);
+        mixer.SetFloat("SfxVol", Mathf.Log10(PlayerPrefs.GetFloat("sfxVol")) * 20);
+        resolutions = Screen.resolutions;
+        resDrop.ClearOptions();
+        List<string> options = new List<string>();
+        int currentResIndex = 0;
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + "x" + resolutions[i].height;
+            options.Add(option);
+
+            if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResIndex = i;
+            }
+        }
+        resDrop.AddOptions(options);
+        resDrop.value = currentResIndex;
+        resDrop.RefreshShownValue();
+    }
+
+    public void SetResolution(int resIndex)
+    {
+        Resolution resolution = resolutions[resIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }*/
+}
