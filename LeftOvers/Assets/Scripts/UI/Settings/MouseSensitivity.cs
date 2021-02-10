58e75c16f6359bdf18f671f8f487adaf48df59ba
JonasLK <49680607+JonasLK@users.noreply.github.com>
2021-02-10 11:42:04 +0100

    .

--- LeftOvers/Assets/Scripts/UI/Settings/MouseSensitivity.cs
+++ LeftOvers/Assets/Scripts/UI/Settings/MouseSensitivity.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class MouseSensitivity : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void SetSens(float sliderValue)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        PlayerPrefs.SetFloat("mouseSens", sliderValue);
     }
 }
