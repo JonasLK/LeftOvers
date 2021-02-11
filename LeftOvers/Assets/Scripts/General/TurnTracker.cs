963c8512fb4b90d46308d8e622ba5772e35f9aec
JonasLK <49680607+JonasLK@users.noreply.github.com>
2021-02-11 15:54:49 +0100

    mm and turn tracker WIP

---
+++ LeftOvers/Assets/Scripts/General/TurnTracker.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnTracker : MonoBehaviour
+{
+    public int playerTurn;
+    public int playerAmount;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void EndTurn()
+    {
+        playerTurn++;
+        if (playerTurn > playerAmount)
+        {
+            playerTurn = 1;
+        }
+    }
+}
