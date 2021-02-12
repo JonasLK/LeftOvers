cd1debdc0be2d88cf70d9279d92b2b2108cef3e4
JonasLK <49680607+JonasLK@users.noreply.github.com>
2021-02-12 15:40:14 +0100

    Pause Menu

---
+++ LeftOvers/Assets/Scripts/General/PauseMenu.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pauseMenu;
+    public GameObject pauseMenuButtons;
+    public GameObject settingsMenu;
+    public bool pauseMenuOpen;
+    public bool settingsMenuOpen;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            if (pauseMenuOpen == false)
+            {
+                OpenPauseMenu();
+                pauseMenuOpen = true;
+            }
+            else if (settingsMenuOpen == true)
+            {
+                settingsMenu.SetActive(false);
+                pauseMenuButtons.SetActive(true);
+                ClosingSettings();
+            }
+            else
+            {
+                ClosePauseMenu();
+                pauseMenuOpen = false;
+            }
+        }
+    }
+
+    public void OpenPauseMenu()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void ClosePauseMenu()
+    {
+        Time.timeScale = 1;
+        pauseMenu.SetActive(false);
+    }
+
+    public void OpeningSettings()
+    {
+        settingsMenuOpen = true;
+    }
+
+    public void ClosingSettings()
+    {
+        settingsMenuOpen = false;
+    }
+}
