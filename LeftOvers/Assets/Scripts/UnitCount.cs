ad2918e1a5ed45abc0d3ba245ce85f077870df6c
Ruben-nn <60340371+Ruben-nn@users.noreply.github.com>
2021-02-19 11:35:25 +0100

    Unit select screen WIP
    unitcounters zijn static en worden overgebracht naar andere scene

--- LeftOvers/Assets/Scripts/UnitCount.cs
+++ LeftOvers/Assets/Scripts/UnitCount.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 public class UnitCount : MonoBehaviour
 {
-    public GameObject[] fighters;
-    GameObject[] archers;
-    GameObject[] mages;
-    GameObject[] priests;
-    GameObject[] paladins;
-    GameObject[] rogues;
+    public float fighters;
+    public float archers;
+    public float mages;
+    public float priests;
+    public float paladins;
+    public float rogues;
 
     public static float fighterCount;
     public static float archerCount;
@@ -23,38 +23,5 @@ public class UnitCount : MonoBehaviour
     public void Start()
     {
 
-    }
-    public void ConfirmUnits()
-    {
-        fighters = GameObject.FindGameObjectsWithTag("FighterUnit");
-        foreach (GameObject fighter in fighters)
-        {
-            fighterCount += 1;
-        }
-        archers = GameObject.FindGameObjectsWithTag("ArcherUnit");
-        foreach (GameObject archer in archers)
-        {
-            archerCount += 1;
-        }
-        mages = GameObject.FindGameObjectsWithTag("mageUnit");
-        foreach (GameObject mage in mages)
-        {
-            mageCount += 1;
-        }
-        priests = GameObject.FindGameObjectsWithTag("priestUnit");
-        foreach (GameObject priest in priests)
-        {
-            priestCount += 1;
-        }
-        paladins = GameObject.FindGameObjectsWithTag("paladinUnit");
-        foreach (GameObject paladin in paladins)
-        {
-            paladinCount += 1;
-        }
-        rogues = GameObject.FindGameObjectsWithTag("rogueUnit");
-        foreach (GameObject rogue in rogues)
-        {
-            rogueCount += 1;
-        }
     }
 }
--- LeftOvers/Assets/Scripts/UnitSelect.cs
+++ LeftOvers/Assets/Scripts/UnitSelect.cs
@@ -19,9 +19,22 @@ public class UnitSelect : MonoBehaviour
     public Text moneyText;
     public Text totalMoney;
 
+    public float fighterCount;
+    public float mageCount;
+    public float archerCount;
+    public float prietsCount;
+    public float paladinCount;
+    public float rogueCount;
+
     void Start()
     {
         moneyLeft = money;
+        fighterCount = 0;
+        mageCount = 0;
+        archerCount = 0;
+        prietsCount = 0;
+        paladinCount = 0;
+        rogueCount = 0;
     }
 
     void Update()
@@ -37,6 +50,7 @@ public class UnitSelect : MonoBehaviour
         {
             moneyLeft -= 1;
             Instantiate(fighterButton, unitPanel.transform);
+            fighterCount += 1;
         }
     }
 
@@ -46,6 +60,7 @@ public class UnitSelect : MonoBehaviour
         {
             moneyLeft -= 1;
             Instantiate(mageButton, unitPanel.transform);
+            mageCount += 1;
         }
     }
 
@@ -55,6 +70,7 @@ public class UnitSelect : MonoBehaviour
         {
             moneyLeft -= 1;
             Instantiate(archerButton, unitPanel.transform);
+            archerCount += 1;
         }
     }
 
@@ -64,6 +80,7 @@ public class UnitSelect : MonoBehaviour
         {
             moneyLeft -= 1;
             Instantiate(priestButton, unitPanel.transform);
+            prietsCount += 1;
         }
     }
 
@@ -73,6 +90,7 @@ public class UnitSelect : MonoBehaviour
         {
             moneyLeft -= 1;
             Instantiate(paladinButton, unitPanel.transform);
+            paladinCount += 1;
         }
     }
 
@@ -82,6 +100,42 @@ public class UnitSelect : MonoBehaviour
         {
             moneyLeft -= 1;
             Instantiate(rogueButton, unitPanel.transform);
+            rogueCount += 1;
         }
+    }
+
+    public void ConfirmUnits()
+    {
+        gameObject.GetComponent<UnitCount>().fighters = fighterCount;
+        gameObject.GetComponent<UnitCount>().mages = mageCount;
+        gameObject.GetComponent<UnitCount>().archers = archerCount;
+        gameObject.GetComponent<UnitCount>().priests = prietsCount;
+        gameObject.GetComponent<UnitCount>().paladins = paladinCount;
+        gameObject.GetComponent<UnitCount>().rogues = rogueCount;
+    }
+
+    public void SellFighter()
+    {
+        fighterCount -= 1;
+    }
+    public void SellMage()
+    {
+        mageCount -= 1;
+    }
+    public void SellArcher()
+    {
+        archerCount -= 1;
+    }
+    public void SellPriest()
+    {
+        prietsCount -= 1;
+    }
+    public void SellPaladin()
+    {
+        paladinCount -= 1;
+    }
+    public void SellRogue()
+    {
+        rogueCount -= 1;
     }
 }
