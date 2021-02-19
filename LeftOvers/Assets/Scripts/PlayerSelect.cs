b7e6440c5ff16de62d3b381eb32a161c861ad654
Ruben-nn <60340371+Ruben-nn@users.noreply.github.com>
2021-02-19 18:23:54 +0100

    PlayerSelect WIP
     ^

---
+++ LeftOvers/Assets/Scripts/PlayerSelect.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSelect : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void Player1()
+    {
+
+    }
+    public void Player2()
+    {
+
+    }
+    public void Player3()
+    {
+
+    }
+}
