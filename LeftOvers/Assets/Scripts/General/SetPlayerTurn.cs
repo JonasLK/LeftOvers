13b6e3e358a896910c57bf724025b63d508edfe5
JonasLK <49680607+JonasLK@users.noreply.github.com>
2021-03-04 22:58:37 +0100

    unit spawn working?

---
+++ LeftOvers/Assets/Scripts/General/SetPlayerTurn.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetPlayerTurn : MonoBehaviour
+{
+    public TurnTracker turnTracker;
+
+    void Start()
+    {
+        turnTracker = GameObject.FindGameObjectWithTag("GameManager").GetComponent<TurnTracker>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetPlayerTurnVoid(int teamTurnToSet)
+    {
+        turnTracker.playerTurn = teamTurnToSet;
+    }
+}
