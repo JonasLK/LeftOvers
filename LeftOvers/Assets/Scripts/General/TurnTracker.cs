90f6443ab3a090b5573f92823f938cdc70ca4462
JonasLK <49680607+JonasLK@users.noreply.github.com>
2021-03-05 09:17:13 +0100

    .

--- LeftOvers/Assets/Scripts/General/TurnTracker.cs
+++ LeftOvers/Assets/Scripts/General/TurnTracker.cs
@@ -6,31 +6,64 @@ public class TurnTracker : MonoBehaviour
 {
     public int playerTurn;
     public int playerAmount;
-    public List<List<GameObject>> playerTeamPlayerUnit;
+    public List<GameObject> team1Unit;
+    public List<GameObject> team2Unit;
+    public List<GameObject> team3Unit;
+    public List<GameObject> team4Unit;
 
 
-    public void AddToListList()
+    public void AddToListList(GameObject gameObjectToAdd)
     {
-
+        if(gameObjectToAdd.GetComponent<TestPlayerMovement>().teamNumber == 1)
+        {
+            team1Unit.Add(gameObjectToAdd);
+        }
+        else if(gameObjectToAdd.GetComponent<TestPlayerMovement>().teamNumber == 2)
+        {
+            team2Unit.Add(gameObjectToAdd);
+        }
+        else if(gameObjectToAdd.GetComponent<TestPlayerMovement>().teamNumber == 3)
+        {
+            team3Unit.Add(gameObjectToAdd);
+        }
+        else if(gameObjectToAdd.GetComponent<TestPlayerMovement>().teamNumber == 4)
+        {
+            team4Unit.Add(gameObjectToAdd);
+        }
     }
 
     public void EndTurn()
     {
-        foreach (List<GameObject> playerTeam in playerTeamPlayerUnit)
+
+        if (playerTurn == 1)
         {
-            if (playerTeam[0].GetComponent<TestPlayerMovement>().teamNumber == playerTurn)
-            {
-                foreach (GameObject playerUnit in playerTeam)
-                {
-                    playerUnit.GetComponent<TestPlayerMovement>().ResetCharacter();
-                }
-            }
+            ResetTeam(team1Unit);
+        }
+        else if (playerTurn == 2)
+        {
+            ResetTeam(team2Unit);
+        }
+        else if (playerTurn == 3)
+        {
+            ResetTeam(team3Unit);
+        }
+        else if (playerTurn == 4)
+        {
+            ResetTeam(team4Unit);
         }
 
         playerTurn++;
         if (playerTurn > playerAmount)
         {
             playerTurn = 1;
+        }
+    }
+
+    public void ResetTeam(List<GameObject> playerTeam)
+    {
+        foreach (GameObject playerUnit in playerTeam)
+        {
+            playerUnit.GetComponent<TestPlayerMovement>().ResetCharacter();
         }
     }
 }
