4d5e22ea1d9adc6356efcf1fd3694b91110f6888
Ruben-nn <60340371+Ruben-nn@users.noreply.github.com>
2021-03-12 12:56:12 +0100

    UI button sounds
    ^

---
+++ LeftOvers/Assets/UI-UX/Sounds/SoundManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SoundManager : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public AudioClip buttonPress;
+
+
+    void Start()
+    {
+        audioSource = gameObject.GetComponent<AudioSource>();
+    }
+
+    public void playButtonSound()
+    {
+        audioSource.PlayOneShot(buttonPress);
+    }
+}
