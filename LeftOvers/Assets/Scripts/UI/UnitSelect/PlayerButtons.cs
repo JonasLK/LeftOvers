73e003d7d599d75d9d11ae165c2d084ac379f07e
JonasLK <49680607+JonasLK@users.noreply.github.com>
2021-03-20 19:05:35 +0100

    build fix wip

--- LeftOvers/Assets/Scripts/UI/UnitSelect/PlayerButtons.cs
+++ LeftOvers/Assets/Scripts/UI/UnitSelect/PlayerButtons.cs
@@ -22,19 +22,26 @@ public class PlayerButtons : MonoBehaviour
         
     }
 
-    public void ClickButton(string playerNumberString)
+    public void ClickButton(int playerNumberString)
     {
-        counter = 0;
+        counter = 1;
         foreach (GameObject playerButton in playerButtons)
         {
-            if(counter < turnTracker.playerAmount)
+            if(counter == playerNumberString)
+            {
+                 playerSelectText.text = "Player " + playerNumberString.ToString() + " units:";
+                playerButton.SetActive(false);
+            }
+            else if(counter <= turnTracker.playerAmount)
             {
                 playerButton.SetActive(true);
+            }
+            else
+            {
+                playerButton.SetActive(false);
             }
 
             counter += 1;
         }
-        playerSelectText.text = "Player " + playerNumberString;
-        gameObject.SetActive(false);
     }
 }
